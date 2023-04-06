8016fcad21273670297435317b1f300b771372f4
nataliarizzi13 <natalia@measurelab.co.uk>
2023-04-06 14:35:16 +0100

    connected bq, code and requirement file updated

--- app.py
+++ app.py
@@ -2,8 +2,18 @@ from dash import Dash, html, dcc, callback, Output, Input
 import dash
 import plotly.express as px
 import pandas as pd
+from google.cloud import bigquery
+from google.oauth2 import service_account
+import db_dtypes
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+
+credentials = service_account.Credentials.from_service_account_file(
+    'snprojectd4a01e34-0cb888d91a40.json')
+
+project_id = 'snprojectd4a01e34'
+client = bigquery.Client(credentials=credentials, project=project_id)
+df = client.query(
+    '''SELECT * FROM snprojectd4a01e34.measurelab_path_to_publish_dashboard.measurelab_aje_submissions_dashboard_permanent''').to_dataframe()
 
 app = dash.Dash(
     __name__,
@@ -12,8 +22,8 @@ app = dash.Dash(
 server = app.server  # Expose server variable for Procfile
 
 app.layout = html.Div([
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    html.H1(children='AJE Visualisation', style={'textAlign':'center'}),
+    dcc.Dropdown(options= df['User_Country'].unique(), id='dropdown-selection'),
     dcc.Graph(id='graph-content')
 ])
 
@@ -22,8 +32,8 @@ app.layout = html.Div([
     Input('dropdown-selection', 'value')
 )
 def update_graph(value):
-    dff = df[df.country==value]
-    return px.line(dff, x='year', y='pop')
+    dff = df[df.User_Country==value]
+    return px.bar(dff, x='Access_Type', y='Word_Count')
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(host="localhost", port="8080", debug=True)
