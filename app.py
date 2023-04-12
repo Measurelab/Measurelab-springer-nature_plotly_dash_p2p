13b705d4b661d50ff1b7eab910bfa4f2add9f3ac
nataliarizzi13 <natalia@measurelab.co.uk>
2023-04-12 15:30:31 +0100

    added sql code and started working on first bar chart

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from dash import Dash, html, dcc, callback, Output, Input
 import dash
 import plotly.express as px
 import pandas as pd
+import numpy as np
 from google.cloud import bigquery
 from google.oauth2 import service_account
 import db_dtypes
@@ -21,10 +22,83 @@ else:
     key_path = "snprojectd4a01e34-0cb888d91a40.json"
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = key_path
 
+scopes = (
+    "https://www.googleapis.com/auth/drive",
+)
+
+bigquery.Client.SCOPE += scopes
 
 client = bigquery.Client(project=project_id)
 df = client.query(
-    '''SELECT * FROM snprojectd4a01e34.measurelab_path_to_publish_dashboard.measurelab_aje_submissions_dashboard_permanent''').to_dataframe()
+    '''
+  WITH main AS (
+  SELECT 
+    user_identity,
+    m.preferred_group_code,
+    m.preferred_group_name,
+    access_type,
+    CASE WHEN grandparent_area_of_study IS NULL THEN "None Provided" ELSE grandparent_area_of_study END as grandparent_area_of_study,
+    CASE WHEN parent_area_of_study IS NULL THEN "None Provided" ELSE parent_area_of_study END as parent_area_of_study ,
+    date(created_at_timestamp) as created_at_date,
+    extract(YEAR from Created_At_Timestamp) as created_at_year,
+    LEFT(CAST(Created_At_Timestamp as STRING),7) as created_at_year_month,
+    --format_datetime("%b %Y",Created_At_Timestamp) as created_at_year_month,
+    File_Name as versions_submitted,
+    word_count,
+    Name_Translation,
+    --NULLIF(REGEXP_REPLACE(REGEXP_REPLACE(LOWER(Name_Translation), '[^a-zA-Z0-9]+', ''), ' ', ''), '') AS  institution_name_match_format -- no spaces
+    NULLIF(REGEXP_REPLACE(LOWER(Name_Translation), r'[^\p{L}\p{N}\s]+', ''), '') AS institution_name_match_format
+
+FROM `snprojectd4a01e34.measurelab_path_to_publish_dashboard.measurelab_aje_submissions_dashboard_permanent` m
+LEFT JOIN `snprojectd4a01e34.measurelab_path_to_publish_plotly_dash.aje_institutions_translated` t
+ON m.preferred_group_code = t.preferred_group_code
+WHERE m.preferred_group_code NOT IN ("Digital Editing Subscription","Digital Translation Subscription","Digital Editing,Digital Translation Subscription")
+)
+,
+lookup_key AS (
+SELECT
+DISTINCT
+  lookup_id,
+  aje_with_space AS institution_match
+FROM `snprojectd4a01e34.measurelab_path_to_publish_dashboard.measurelab_kk_institution_lookup`
+)
+
+SELECT
+DISTINCT 
+    user_identity,
+    m.preferred_group_code,
+    m.preferred_group_name AS institution_name,
+    access_type,
+    grandparent_area_of_study,
+    parent_area_of_study,
+    created_at_date,
+    created_at_year,
+    created_at_year_month,
+    versions_submitted,
+    name_translation,
+    word_count,
+    institution_name_match_format,
+    institution_match,
+REGEXP_REPLACE(CAST(TO_BASE64(MD5(NULLIF(REGEXP_REPLACE(REGEXP_REPLACE(LOWER(COALESCE(lk.lookup_id,institution_name_match_format)), r'[^a-zA-Z0-9]+', ''), ' ', ''), ''))) AS STRING), r'[^a-zA-Z\\d\\s]', '') AS md5_contract
+FROM main m
+LEFT JOIN lookup_key lk
+ON m.institution_name_match_format = lk.institution_match
+''').to_dataframe()
+
+#  `snprojectd4a01e34.measurelab_path_to_publish_plotly_dash.aje_institutions_translated` t
+
+# `snprojectd4a01e34.measurelab_path_to_publish_dashboard.measurelab_kk_institution_lookup`
+
+
+df['user_identity'].nunique()
+
+# AJE - Main Scorecards - Versions Submitted - the unique count of versions_submitted (field name in the main ds 'file_name')
+df['versions_submitted'].nunique()
+
+# AJE - Main Scorecards - Average Word Count - the average/mean of 'word_count'
+df['word_count'].mean()
+rbm = df.groupby('created_at_year_month')['user_identity'].nunique(
+).reset_index().sort_values('created_at_year_month')
 
 app = dash.Dash(
     __name__,
@@ -33,18 +107,22 @@ app = dash.Dash(
 server = app.server  # Expose server variable for Procfile
 
 app.layout = html.Div([
-    html.H1(children='AJE Visualisation', style={'textAlign':'center'}),
-    dcc.Dropdown(options= df['User_Country'].unique(), id='dropdown-selection'),
+    html.H1(children='AJE Visualisation', style={'textAlign': 'center'}),
+    dcc.Dropdown(options=df['created_at_year_month'], value = df['created_at_year_month'][0], id='dropdown-selection',clearable=False),
     dcc.Graph(id='graph-content')
 ])
+
 
 @callback(
     Output('graph-content', 'figure'),
     Input('dropdown-selection', 'value')
 )
-def update_graph(value):
-    dff = df[df.User_Country==value]
-    return px.bar(dff, x='Access_Type', y='Word_Count')
+def update_bar_chart(value):
+    dff = df[df.created_at_year_month == value]
+    fig_1 = px.bar(dff, x='created_at_year_month', y='user_identity', text_auto=True, barmode="group", labels={'created_at_year_month': 'Year Month', 'user_identity': 'Unique Users'}, template='plotly_white'
+                   ).update_traces(marker=dict(color='#192c55'))
+    return fig_1
+
 
 if __name__ == '__main__':
     app.run_server(host="localhost", port="8080", debug=True)
